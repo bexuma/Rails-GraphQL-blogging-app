c2fb8da7111105e6b2f9057bb2ccf3b8fad32f78
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2018-05-08 09:47:32 +0900

    Users validation, User model tests, signup login pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+	before_save { email.downcase! }
 	has_secure_password
 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
 	has_many :posts
-	validates :name, presence: true
-  	validates :email, presence: true, uniqueness: true
+	validates :name, presence: true, length: { maximum: 50 }
+  	validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 },
+  				      format: { with: VALID_EMAIL_REGEX }
+ 	validates :password, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/login'
+  root 'static_pages#homepage'
   resources :posts
+  get  'users/signup',  to: 'users#new'
+  resources :users
+
+
   if Rails.env.development?
       mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
     end
